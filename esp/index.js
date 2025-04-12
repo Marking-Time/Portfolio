fbf6cda0f26ab1475f26aef7461701e4300f8e07
Mark  King <g34519838@gmail.com>
2025-04-12 09:38:48 -0700

    go live

---
+++ esp/index.js
@@ -0,0 +1,35 @@
+// functions called from card to display "read more" content
+function simple() {
+	document.getElementById("simple").style.display = "block";
+}
+function templates(){
+	document.getElementById("templates").style.display = "block";
+}
+function complex(){
+	document.getElementById("complex").style.display = "block";
+}
+function data(){
+	document.getElementById("data").style.display = "block";
+}
+function talk(){
+	document.getElementById("talk").style.display = "block";
+}
+
+// functions called from content to hide content
+function simple_top(){
+	document.getElementById("simple").style.display = "none";
+}
+function templates_top(){
+	document.getElementById("templates").style.display = "none";
+	document.getElementById("templates").style.display = "none";
+
+}
+function complex_top(){
+	document.getElementById("complex").style.display = "none";
+}
+function data_top(){
+	document.getElementById("data").style.display = "none";
+}
+function talk_top(){
+	document.getElementById("talk").style.display = "none";
+}
